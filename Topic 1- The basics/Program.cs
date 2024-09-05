8b71d3e857e245772531fba017a50416605d3f33
339710998 <339710998@2260I-L29-027>
2024-09-05 09:30:14 -0400

    Add project files.

---
+++ Topic 1- The basics/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Topic_1__The_basics
+{   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Liam Day
+           Console.WriteLine("Hello, World!");
+            Console.WriteLine("Goodmorning World");
+            Console.WriteLine("i will write on the next line");
+            Console.WriteLine("");
+            Console.ReadLine();
+            Console.Write("Hello World");
+            Console.Write("what,s different");
+        }        
+        
+    }
+}
